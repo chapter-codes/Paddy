0082d51c69ffc6d7c4b2a0b5b911d73dcbcc7fb0
churchill nnanna <mrxyfactor@gmail.com>
2023-12-15 15:18:18 +0100

    push from local machine

---
+++ src/app/@chats/page.tsx
@@ -0,0 +1,12 @@
+import {Metadata} from 'next'
+const metadata:Metadata={
+    title:'chats'
+}
+
+export default function Chats(){
+    return(
+        <div className="h-screen bg-black text-white">
+            <h1>Chats</h1>
+        </div>
+    )
+} 
---
+++ src/app/@video/page.tsx
@@ -0,0 +1,12 @@
+import {Metadata} from 'next'
+const metadata:Metadata={
+    title:'video'
+}
+
+export default function Chats(){
+    return(
+        <div className="h-screen bg-blsck text-white">
+            <h1>video</h1>
+        </div>
+    )
+} 
